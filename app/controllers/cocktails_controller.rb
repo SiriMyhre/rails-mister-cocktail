9d31e9c25a2739c153d39ed0521588295bdc3454
Siri Myhre <siri.o.myhre@gmail.com>
2018-08-02 15:11:17 +0100

    finished the controller for cocktail and some styling

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,9 +10,18 @@ class CocktailsController < ApplicationController
   end
 
   def new
-
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
